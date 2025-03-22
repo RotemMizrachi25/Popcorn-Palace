bea68c849c9d40a321633f78a384dbd3834c30bd
Rotem Mizrachi <rotem.mizrachi@post.runi.ac.il>
2025-03-22 17:27:11 +0200

    Update app.module.ts with database and module configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { ShowtimesModule } from './showtimes/showtimes.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'popcorn-palace',
+      password: 'popcorn-palace',
+      database: 'popcorn-palace',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    MoviesModule,
+    ShowtimesModule,
+    BookingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
